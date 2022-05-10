e039ad287a968eda0c0cb40f52f99f5211ebe2d7
scott-carew <carew.scott@gmail.com>
2022-05-10 13:44:19 +1200

    Complete if else statements

--- inflate-the-unicorn/inflate-the-unicorn.js
+++ inflate-the-unicorn/inflate-the-unicorn.js
@@ -1,5 +1,33 @@
-  // -    -   -   -   -  //
- // JAVASCRIPT CARNIVAL //
+// -    -   -   -   -  //
+// JAVASCRIPT CARNIVAL //
 // -    -   -   -   -  //
 
-console.log("Inflate The Unicorn!")
+console.log('Inflate The Unicorn!')
+
+//Get an array containing the unicorns
+
+let images = document.getElementsByClassName('inflate-an-image')
+// console.log(images)
+
+//Bind the onclick method to our unicorns
+for (let i = 0; i < images.length; i++) {
+  images[i].onclick = imageClicked
+}
+
+//Write a function that gets called every time you click on a unicorn
+
+function imageClicked(e) {
+  //Create a variable for the clicked image so we can do something with it.
+  let image = e.target
+  //Change image 0 to be image 1
+  if (image.src.match('./images/unicorn-0.png')) {
+    image.src = './images/unicorn-1.png'
+  } else if (image.src.match('./images/unicorn-1.png')) {
+    image.src = './images/unicorn-2.png'
+  } else if (image.src.match('./images/unicorn-2.png')) {
+    image.src = './images/unicorn-3.png'
+  } else {
+    alert('The Unicorn says thank you')
+  }
+}
+// console.log(image)

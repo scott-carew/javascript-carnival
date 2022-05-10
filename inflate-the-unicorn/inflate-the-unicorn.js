a3e996454d438d33414dc73f89f9bf9097a1621f
scott-carew <carew.scott@gmail.com>
2022-05-10 14:59:32 +1200

    Add unicorn number

--- inflate-the-unicorn/inflate-the-unicorn.js
+++ inflate-the-unicorn/inflate-the-unicorn.js
@@ -7,9 +7,9 @@ console.log('Inflate The Unicorn!')
 //Get an array containing the unicorns
 
 let images = document.getElementsByClassName('inflate-an-image')
-// console.log(images)
+// // console.log(images)
 
-//Bind the onclick method to our unicorns
+// //Bind the onclick method to our unicorns
 for (let i = 0; i < images.length; i++) {
   images[i].onclick = imageClicked
 }
@@ -19,6 +19,7 @@ for (let i = 0; i < images.length; i++) {
 function imageClicked(e) {
   //Create a variable for the clicked image so we can do something with it.
   let image = e.target
+
   //Change image 0 to be image 1
   if (image.src.match('./images/unicorn-0.png')) {
     image.src = './images/unicorn-1.png'
@@ -27,7 +28,12 @@ function imageClicked(e) {
   } else if (image.src.match('./images/unicorn-2.png')) {
     image.src = './images/unicorn-3.png'
   } else {
-    alert('The Unicorn says thank you')
+    if (image.id == 'one') {
+      alert('Unicorn Number 1 says thank you!')
+    } else if (image.id == 'two') {
+      alert('Unicorn Number 2 says thank you!')
+    } else if (image.id == 'three') {
+      alert('Unicorn Number 3 says thank you!')
+    }
   }
 }
-// console.log(image)
